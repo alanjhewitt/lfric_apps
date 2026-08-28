f64ece62e8c266b9962b92a9b24f13097640361f
Alan J Hewitt <40797340+alanjhewitt@users.noreply.github.com>
2026-08-28 10:36:47 +0100

    Applied upgrade macro

--- applications/jedi_lfric_tests/rose-meta/jedi_forecast/versions.py
+++ applications/jedi_lfric_tests/rose-meta/jedi_forecast/versions.py
@@ -61,3 +61,27 @@ class vn32_t634(MacroUpgrade):
         self.add_setting(config, [nml, "init_sort_theta"], ".true.")
 
         return config, self.reports
+
+
+class vn32_t698(MacroUpgrade):
+    """Upgrade macro for ticket #698 by Alan J Hewitt."""
+
+    BEFORE_TAG = "vn3.2_t634"
+    AFTER_TAG = "vn3.2_t698"
+
+    def upgrade(self, config, meta_config=None):
+        # Commands From: rose-meta/um-aerosol
+        # Add new settings with the default option SUBCOCSSDU_7mode
+        self.add_setting(
+            config, ["namelist:aerosol", "mode_setup"], "'SUBCOCSSDU_7mode'"
+        )
+        # Default to false since this is the setting in all existing tests
+        self.add_setting(
+            config, ["namelist:aerosol", "l_dust_mp_ageing"], ".false."
+        )
+        # Default to true since this is the setting in all existing tests
+        self.add_setting(
+            config, ["namelist:aerosol", "l_ukca_radaer_sustrat"], ".true."
+        )
+
+        return config, self.reports
--- interfaces/physics_schemes_interface/rose-meta/um-aerosol/versions.py
+++ interfaces/physics_schemes_interface/rose-meta/um-aerosol/versions.py
@@ -19,37 +19,24 @@ class UpgradeError(Exception):
 
 
 class vn32_t698(MacroUpgrade):
-    """
-    Upgrade macro for ticket #698 by Alan J Hewitt.
-
-    Users can now select UKCA GLOMAP setting via namelist.
-    Note that dust_and_clim is treated by UKCA as if setting (6)
-    but is treated by RADAER as if setting (8).
-
-    Users can now set dust ageing via the namelist.
-
-    Users can now set radaer SUstrat via the namelist.
-    """
+    """Upgrade macro for ticket #698 by Alan J Hewitt."""
 
     BEFORE_TAG = "vn3.2"
     AFTER_TAG = "vn3.2_t698"
 
     def upgrade(self, config, meta_config=None):
-        # Add settings
-
+        # Commands From: rose-meta/um-aerosol
         # Add new settings with the default option SUBCOCSSDU_7mode
-        self.add_setting( config,
-                          ["namelist:aerosol","mode_setup"],
-                          "'SUBCOCSSDU_7mode'")
-
+        self.add_setting(
+            config, ["namelist:aerosol", "mode_setup"], "'SUBCOCSSDU_7mode'"
+        )
         # Default to false since this is the setting in all existing tests
-        self.add_setting( config,
-                          ["namelist:aerosol", "l_dust_mp_ageing"],
-                          ".false." )
-
+        self.add_setting(
+            config, ["namelist:aerosol", "l_dust_mp_ageing"], ".false."
+        )
         # Default to true since this is the setting in all existing tests
-        self.add_setting( config,
-                          ["namelist:aerosol", "l_ukca_radaer_sustrat"],
-                          ".true." )
+        self.add_setting(
+            config, ["namelist:aerosol", "l_ukca_radaer_sustrat"], ".true."
+        )
 
         return config, self.reports
